992c7103f2a22177905b69506f46fcf5b5dbb06c
MOHAMAD AGHI <87778581+aghimohamad@users.noreply.github.com>
2024-12-30 20:29:00 +0300

    Initialize patch/Add-useBehaviorSubject-hook

---
+++ dotnet/GreenOnSoftware/GreenOnSoftware.Application/Reviews/UpdateReviewCommand/UpdateReviewHandler.cs
@@ -0,0 +1,49 @@
+﻿using GreenOnSoftware.Commons.Dtos;
+using GreenOnSoftware.Commons.Clock;
+using GreenOnSoftware.Commons.Resources;
+using GreenOnSoftware.DataAccess;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using GreenOnSoftware.Commons.Context;
+using GreenOnSoftware.Application.Reviews.AddReviewCommand;
+using GreenOnSoftware.Core.Models.Reviews;
+
+namespace GreenOnSoftware.Application.Reviews.UpdateReviewCommand;
+
+internal class UpdateReviewHandler : IRequestHandler<UpdateReview, Result>
+{
+    private readonly GreenOnSoftwareDbContext _dbContext;
+    private readonly IClock _clock;
+    private readonly IContext _context;
+
+    public UpdateReviewHandler(IClock clock, GreenOnSoftwareDbContext dbContext, IContext context)
+    {
+        _clock = clock;
+        _dbContext = dbContext;
+        _context = context;
+    }
+
+    public async Task<Result> Handle(UpdateReview command, CancellationToken cancellationToken)
+    {
+        var result = new Result<UpdateReviewResult>();
+
+        var currentArticle = await _dbContext.Articles
+            .Include(x=>x.Reviews)
+            .SingleOrDefaultAsync(x => !x.IsDeleted && x.Id == command.ArticleId);
+
+        if (currentArticle is null)
+        {
+            result.AddErrorWithLogging(ErrorMessages.ArticleNotFound);
+            return result;
+        }
+
+        currentArticle.UpdateReview(command.ReviewId, command.Content, _context.Identity.Id!, _clock.UtcNow);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        result.SetData(new UpdateReviewResult {
+            Id = command.ReviewId
+        });
+
+        return result;
+    }
+}
---
+++ system/apps/blog/store/article-management/actions.ts
@@ -0,0 +1,40 @@
+import { useArticleManagementStore } from './store';
+import type * as ArticleManagement from './defs';
+import { article_reviews_actions } from '../article-reviews';
+import { article_actions } from '../article/actions';
+import { add_article_review_actions } from '../add-article-review';
+import { mockArticleReview } from '@system/blog-api-mocks';
+import { change_article_status_actions } from '../change-article-status';
+
+const { setState: set } = useArticleManagementStore;
+
+const article_management_actions: ArticleManagement.Actions = {
+  start: (id, url, lang) => {
+    set({ is: 'active', id });
+    article_reviews_actions.load(id);
+    article_actions.load({ url, lang });
+  },
+  reset: () => {
+    set({ is: 'idle' });
+  },
+  confirm: async (id) => {
+    await add_article_review_actions.confirm(id);
+    // @TODO: Remove mock when backend will finish work.
+    article_reviews_actions.addReview(mockArticleReview());
+  },
+  changeStatus: async (id, status) => {
+    if (status === 'Accepted') {
+      await change_article_status_actions.accept(id);
+      article_actions.update({ status });
+      return;
+    }
+
+    if (status === 'NeedWork') {
+      await change_article_status_actions.reject(id);
+      article_actions.update({ status });
+      return;
+    }
+  },
+};
+
+export { article_management_actions };
