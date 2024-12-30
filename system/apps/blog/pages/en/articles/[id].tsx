3ebb4d88be3a14d03787edcd8d913bb90448765b
MOHAMAD AGHI <87778581+aghimohamad@users.noreply.github.com>
2024-12-30 20:28:13 +0300

    Initialize release/v1.3

---
+++ system/apps/blog/pages/en/articles/[id].tsx
@@ -0,0 +1,50 @@
+import type { GetStaticProps, GetStaticPaths } from 'next';
+import type { ArticlePageParams, ArticlePageProps } from '../../../models';
+
+import { createArticlePath, getArticlesIds } from '../../../utils';
+import { readFileSync } from 'fs';
+import { serialize } from 'next-mdx-remote/serialize';
+import { MainLayout, PageWrapper } from '../../../components';
+import { MDXRemote } from 'next-mdx-remote';
+import { ArticleLayout } from '@system/figa-ui';
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const ids = await getArticlesIds('en');
+
+  return {
+    paths: ids.map((id) => ({
+      params: {
+        id,
+      },
+    })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<ArticlePageProps> = async ({
+  params,
+}: ArticlePageParams) => {
+  const filePath = createArticlePath('en', params.id + '.mdx');
+  const source = await serialize(readFileSync(filePath, 'utf8'));
+
+  return {
+    props: {
+      id: params.id,
+      source,
+    },
+  };
+};
+
+const ArticlePage = ({ source }: ArticlePageProps) => {
+  return (
+    <MainLayout>
+      <ArticleLayout>
+        <PageWrapper>
+          <MDXRemote {...source} />
+        </PageWrapper>
+      </ArticleLayout>
+    </MainLayout>
+  );
+};
+
+export default ArticlePage;
