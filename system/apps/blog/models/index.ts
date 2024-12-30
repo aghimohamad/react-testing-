6f9dd0fcfdf4f8bebb42db105152198491f1b8c5
MOHAMAD AGHI <87778581+aghimohamad@users.noreply.github.com>
2024-12-30 20:28:40 +0300

    Initialize lesson/Creating-fixture-for-MSW-to-reduce-boilerplate-and-setup

---
+++ system/apps/blog/models/index.ts
@@ -0,0 +1,29 @@
+import type { ArticleDto, FullArticleDto, Url } from '@system/blog-api-models';
+import type { MDXRemoteSerializeResult } from 'next-mdx-remote';
+
+interface ArticlePageProps {
+  url: Url;
+  mdx: MDXRemoteSerializeResult;
+  article: FullArticleDto;
+}
+
+interface ArticlePageParams {
+  params: {
+    url: Url;
+  };
+}
+
+interface ArticlesPageProps {
+  articles: ArticleDto[];
+}
+
+interface YourArticesPageProps {
+  articles: ArticleDto[];
+}
+
+export type {
+  ArticlePageParams,
+  ArticlePageProps,
+  ArticlesPageProps,
+  YourArticesPageProps,
+};
