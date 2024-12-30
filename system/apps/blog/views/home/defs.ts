2f1156baa59c0cfa1fe0818df7851f8f7240cb1a
MOHAMAD AGHI <87778581+aghimohamad@users.noreply.github.com>
2024-12-30 20:28:11 +0300

    Initialize feature/main-branch-deployment-trigger

---
+++ system/apps/blog/views/home/defs.ts
@@ -0,0 +1,7 @@
+import { ArticleModel } from '@system/blog-models';
+
+interface HomeViewProps {
+  articles: ArticleModel[];
+}
+
+export type { HomeViewProps };
---
+++ system/libs/utils/src/lib/utils.ts
@@ -0,0 +1,5 @@
+const utils = (): string => {
+  return 'utils';
+};
+
+export { utils };
